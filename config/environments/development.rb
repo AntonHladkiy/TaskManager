cfe90f3cc50a56e80a7876f52b73f1aab8c9a904
Anton Hladkyi <gladkyy@ukr.net>
2021-01-31 14:52:03 +0200

    .

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,25 +28,25 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-
   #config.action_mailer.perform_deliveries = true
   #config.action_mailer.raise_delivery_errors = true
-  #config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   #config.action_mailer.delivery_method = :smtp
-  #config.action_mailer.smtp_settings = {
-  #user_name:      ENV['email'],
-  #password:       ENV['password'],
-  # domain:         'localhost:3000',
-  # address:       'smtp.gmail.com',
-  # port:          '587',
-  # authentication: :plain,
-  # enable_starttls_auto: true
-  #}
+  #config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  user_name:      ENV['email'],
+  password:       ENV['password'],
+   domain:         'localhost:3000',
+   address:       'smtp.gmail.com',
+   port:          '587',
+   authentication: :plain,
+   enable_starttls_auto: true
+  }
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,10 @@
 ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :address              =>  'smtp.sendgrid.net',
-  :port                 =>  '587',
-  :authentication       =>  :plain,
-  :user_name            =>  'app199835451@heroku.com',
-  :password             =>  'mow0de0o9359',
-  :domain               =>  'heroku.com',
-  :enable_starttls_auto  =>  true
-}
+#ActionMailer::Base.smtp_settings = {
+# :address              =>  'smtp.sendgrid.net',
+# :port                 =>  '587',
+#:authentication       =>  :plain,
+#:user_name            =>  'app199835451@heroku.com',
+#  :password             =>  'mow0de0o9359',
+#  :domain               =>  'heroku.com',
+# :enable_starttls_auto  =>  true
+#}
