7242581a28366fbf2a836edd38f899c1311684fa
Anton Hladkyi <gladkyy@ukr.net>
2021-01-31 14:58:52 +0200

    could make sendgrid working so i deleted option to send verification emails to demonstrate rest of the app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   has_many :tasks
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable :rememberable,:recoverable,
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable :rememberable,:recoverable,,:confirmable
   devise :database_authenticatable, :registerable,
-         :validatable,:confirmable
+         :validatable
   def full_name
     return firstName+secondName;
   end
