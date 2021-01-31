1d1237b3a130d3905457765244a7a24ed988010c
Anton Hladkyi <gladkyy@ukr.net>
2021-01-31 14:41:01 +0200

    .

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
   :address              =>  'smtp.sendgrid.net',
   :port                 =>  '587',
   :authentication       =>  :plain,
-  :user_name            =>  'gladkyy.an@gmail.com',
-  :password             =>  'antonantonanton3015',
+  :user_name            =>  'app199835451@heroku.com',
+  :password             =>  'mow0de0o9359',
   :domain               =>  'heroku.com',
   :enable_starttls_auto  =>  true
 }
