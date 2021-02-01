ceb1c6a02a48a0c693e7df7eed790aafd25458ae
Anton Hladkyi <gladkyy@ukr.net>
2021-02-01 08:31:19 +0200

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   has_many :tasks
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable :rememberable,:recoverable,,:confirmable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable :rememberable,:recoverable,
   devise :database_authenticatable, :registerable,
-         :validatable
+         :validatable,:confirmable
   def full_name
     return firstName+secondName;
   end
